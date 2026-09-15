52b12921a3318b1619351760a595d204544b9bf3
Lucas Queiroz <lucasouqueiroz@gmail.com>
2026-09-14 22:07:45 -0300

    Configura vite para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
-  base: './'
-});
+  base: '/gestao-financeira-app/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
+})
